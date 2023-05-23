31c7fe6a83002e555fed4fb34a0c15bc6433b7b4
Siddardha-code <85091100+Siddardha-code@users.noreply.github.com>
2023-05-23 12:56:29 +0530

    Update ProductService.java

--- FinindingProductUsingSPRING/ProductService.java
+++ FinindingProductUsingSPRING/ProductService.java
@@ -1,5 +1,3 @@
-package Telesko.FinindingProductUsingSPRING;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,34 +5,18 @@ import java.util.List;
 
 @Service
 public class ProductService {
-    private final ProductRepository productRepository;
-
     @Autowired
-    public ProductService(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
-
-    public void addProduct(Product product) {
-        productRepository.save(product);
-    }
+    private ProductRepository productRepository;
 
     public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
 
-    public Product getProduct(String place) {
-        return productRepository.findByPlace(place);
-    }
-
-    public List<Product> getProductWithText(String text) {
-        return productRepository.findByNameContainingIgnoreCaseOrTypeContainingIgnoreCaseOrPlaceContainingIgnoreCase(text, text, text);
+    public List<Product> getProductsWithText(String text) {
+        return productRepository.findByNameContainingIgnoreCase(text);
     }
 
     public List<Product> getOutOfWarrantyProducts(int warrantyYear) {
         return productRepository.findByWarrantyLessThan(warrantyYear);
-    }
-
-    public List<Product> getProductsWithPlace(String place) {
-        return productRepository.findByPlace(place);
     }
 }
