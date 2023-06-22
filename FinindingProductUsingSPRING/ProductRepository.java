0f993778dde59efc1c4eb5cdfa43e71e855cc3d6
Siddardha-code <85091100+Siddardha-code@users.noreply.github.com>
2023-06-22 10:59:17 +0530

    Delete FinindingProductUsingSPRING directory

--- FinindingProductUsingSPRING/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-    List<Product> findByNameContainingIgnoreCase(String text);
-    List<Product> findByWarrantyLessThan(int warrantyYear);
-}
